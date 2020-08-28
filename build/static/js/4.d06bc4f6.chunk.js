ac57e2358e27e887c188cdc6b0e4d2bb957234de
Dexter Hill <49513414+DexterHill0@users.noreply.github.com>
2020-08-28 13:33:46 +0100

    Removed folders

--- build/static/js/4.d06bc4f6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmagic-light"]=this["webpackJsonpmagic-light"]||[]).push([[4],{192:function(n,t,i){"use strict";i.r(t),i.d(t,"startStatusTap",(function(){return o}));var e=i(22),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.f)((function(){var t=n.innerWidth,i=n.innerHeight,o=document.elementFromPoint(t/2,i/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.n)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=4.d06bc4f6.chunk.js.map
